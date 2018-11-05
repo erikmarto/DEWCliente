cb517f6d038701b3ec1bb221034fe777b5400a75
erikmarto <emartorell2@iesfuentesanluis.org>
2018-11-05 11:44:51 +0100

    Arrastrar figuras (Niños)

---
+++ Gulp/arrastrar/js/source/arrastrar.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+    document.getElementById("divCir").ondrop = drop;
+    document.getElementById("divCir").ondragover = allowDrop;
+    document.getElementById("dragCir").ondragstart = drag;
+}
+
+
+
+allowDrop = (ev) => {
+    ev.preventDefault();
+}
+
+drag = (ev) => {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+drop = (ev) => {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+class Figuras{
+    constructor(){
+        this.circulo = false;
+        this.triangulo = false;
+        this.cuadrado = false;
+    }
+}
