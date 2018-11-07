1b3ae929049cf51d29ae1f08402772483cdce889
erikmarto <emartorell2@iesfuentesanluis.org>
2018-11-07 11:26:46 +0100

    arrastrar

--- Gulp/arrastrar/js/source/arrastrar.js
+++ Gulp/arrastrar/js/source/arrastrar.js
@@ -1,27 +1,34 @@
 window.onload = function(){
-    document.getElementById("divCir").ondrop = drop;
-    document.getElementById("divCir").ondragover = allowDrop;
-    document.getElementById("dragCir").ondragstart = drag;
-    document.getElementById("empezar").addEventListener("click", empezar, false);
+    document.getElementById("divCir").addEventListener('ondragover', allowDrop);
+    document.getElementById("divCir").addEventListener('ondrop', drop);
+    document.getElementById("dragCir").addEventListener('ondragstart', drag);
+    
+    document.getElementById("empezar").addEventListener('click', empezar, false);
+
     document.getElementById("container").style.display = "none";
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+    
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+    
+    function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+    }
+
 }
 
-allowDrop = (ev) => {
-    ev.preventDefault();
-}
 
-drag = (ev) => {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-drop = (ev) => {
-    ev.preventDefault();
-    const data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
 
 empezar = () =>{
     document.getElementById("container").style.display = "block";
     document.getElementById("empezar").style.display = "none";
 }
+
+
 
