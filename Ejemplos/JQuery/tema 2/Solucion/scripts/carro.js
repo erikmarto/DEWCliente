fee374d0b2e298bbb8bd8aa142e2298c5b4fd313
Erik Martorell Gomez <erikmarto12@gmail.com>
2018-12-17 08:58:16 +0100

    jQuery

---
+++ Ejemplos/JQuery/tema 2/Solucion/scripts/carro.js
@@ -0,0 +1,79 @@
+﻿function actualizaStockProducto($item, incremento)
+{
+	var actualizado = false;
+	var stock = parseInt($item.children(".stock").html().replace("Stock ", ""));
+	
+	if (stock+incremento >= 0)
+	{
+		stock += incremento;
+		$item.children(".stock").html("Stock " + stock);
+		if (stock == 0)
+			$item.find(".stock").addClass("agotado");
+		else
+			$item.find(".stock").removeClass("agotado");
+			
+		actualizado = true;
+	}
+	
+	return actualizado;
+}
+
+function actualizaNumeroProductosPedidos(incremento)
+{
+	var numProductosPedido = parseInt($("#citem").val());
+	numProductosPedido += incremento;
+	$("#citem").val(numProductosPedido);
+}
+
+function actualizaPrecioTotal($item, incremento)
+{
+	var precioPedido = parseInt($("#cprice").val());
+	precioPedido += parseInt(incremento);
+	$("#cprice").val(precioPedido + " €");
+}
+
+function eliminaProductoDelCarrito($item)
+{
+	var id = $item.attr("id");
+	id = id.substring(1);
+	
+	actualizaStockProducto($("#"+id), 1);
+	
+	actualizaNumeroProductosPedidos(-1);
+	
+	actualizaPrecioTotal($item, -parseInt($item.children(".price").html()));
+	
+	$item.remove();
+}
+
+function anyadeProductoAlCarrito($item)
+{
+	var $delete = $('<a href="" class="delete"></a>');
+
+	$delete.click(function (evento)
+	{
+		eliminaProductoDelCarrito($(this).parent());
+		return false;
+	});
+	
+	var id = "c"+$item.attr("id");
+	$copia = $item.clone().attr("id", id).addClass('icart').prepend($delete);
+	$copia.children(":not(a)").andSelf().css("cursor", "default").find(".stock").hide();
+	
+	$("#cart_items").prepend($copia);
+}
+
+$(function() 
+{
+	$(".item").dblclick(function()
+	{
+		if (actualizaStockProducto($(this), -1) == true)
+		{
+			actualizaNumeroProductosPedidos(1);
+			
+			actualizaPrecioTotal($(this), parseInt($(this).children(".price").html()));
+			
+			anyadeProductoAlCarrito($(this));
+		}
+	});
+});
--- Ejercicios Jquery/Carrito2/scripts/carro.js
+++ Ejercicios Jquery/Carrito2/scripts/carro.js
@@ -1,61 +1,44 @@
-$(function () {
-	sumarItem = false;
+$(function() {
+
+    sumarItem  = false;
 	compras = 0;
-
-	//resteblacer valores a 0 para firefox
-	$("#cprice").val(0 + '€');
-	$("#citem").val(0);
-
-	//movimiento y anchos
-	anchoInicial = 483; //medida
-	ancho = parseInt($("#cart_items").css("width")); //recoger el ancho
-	cantArt = $("#cart_items").children().length; //cantidad de articulos
-	left = parseInt($("#cart_items").css("left"));
-
-	$("div.item").on("dblclick", dblClick); //doble click
-	$("#btn_clear").on("click", vaciarCarrito); //vaciar
 	
-	$("#btn_next").on("click", movDerecha);  //mover derecha
-	$("#btn_prev").on("click", movIquierda);  //mover izquierda
-	
+    $("div.item").dblclick(function() {
+        const itemID = this.id;
+        let stockLabel = $(this).children("label.stock").text();
+        let stock = parseInt(stockLabel.substring(6)); //cantidad del stock 
+        
+        if (stock > 0) {//en caso que sea mayor a 0
+            sumarItem = true;
+            actualizarStock(itemID, stock, sumarItem); //actualizar stock
+			compras++; //sumar coimpras
+
+			/*llamada a las funciones*/
+            actualizarCompra(compras);
+            actualizarTotal(itemID, sumarItem);
+            addCarrito(itemID);
+        } 
+    });
 });
 
-//Funcion doble click
-function dblClick() {
-	const itemID = this.id;
-	let stockLabel = $(this).children("label.stock").text();
-	let stock = parseInt(stockLabel.substring(6)); //cantidad del stock 
-
-	if (stock > 0) {//en caso que sea mayor a 0
-		sumarItem = true;
-		actualizarStock(itemID, stock, sumarItem); //actualizar stock
-		compras++; //sumar coimpras
-
-		/*llamada a las funciones*/
-		actualizarCompra(compras);
-		actualizarTotal(itemID, sumarItem);
-		addCarrito(itemID, cantArt);
-	}
-}
-
-//Funciones flecha
 actualizarStock = (itemID, stock, sumarItem) => {
 	const item = $("#" + itemID);
 
 	if (sumarItem) { //estemos añadiendo o borrándolo un item
-		stock--; //restamos stock 
-
+		stock --; //restamos stock 
+		
 		if (stock < 1) { //si esta a 0 se añade clase
 			item.children("label.stock").addClass("agotado");
-		}
+		}		
 	} else {
-		stock++; //sumamos stock
-
+		stock ++; //sumamos stock
+		
 		if (stock > 0) {
 			item.children("label.stock").removeClass("agotado");
-		}
+		}	
 	}
 	item.children("label.stock").text("Stock " + stock); //rectificamos el valor
+	//console.log(stock);
 }
 
 actualizarCompra = (compras) => {
@@ -63,18 +46,18 @@ actualizarCompra = (compras) => {
 }
 
 actualizarTotal = (itemID, sumarItem) => {
-	const item = $("#" + itemID);
+	const item = $("#" + itemID); 
 	let precioItem = parseInt(item.children(".price").text());
-	let precioTotal = parseInt($("#cprice").val());
+	let precioTotal = parseInt ($("#cprice").val());
 
-	if (sumarItem) { //si añadimos un articulo al carro
-		$("#cprice").val((precioTotal + precioItem) + '€'); //suma precio total 
-	} else {
-		$("#cprice").val((precioTotal - precioItem) + '€'); //resta precio total
+	if (sumarItem){ //si añadimos un articulo al carro
+		$("#cprice").val((precioTotal + precioItem) + '€' ); //suma precio total 
+	}else{
+		$("#cprice").val((precioTotal - precioItem) + '€' ); //resta precio total
 	}
 }
 
-addCarrito = (itemID, cantArt) => {
+addCarrito = (itemID) => {
 	let item = $("#" + itemID);
 	const clon = item.clone(); //crear clon del articulo
 	const $borrar = $('<a href="" class="delete"></a>'); //crear enlace borrar
@@ -85,75 +68,24 @@ addCarrito = (itemID, cantArt) => {
 	$(clon).children(".stock").hide(); //ocultar el stock
 	$(clon).prepend($borrar); //se le añade el enlace al clon
 	$(clon).appendTo($("#cart_items")); //añadir clon al carro
-
+	
 	/*click de borrar*/
-	$borrar.on("click", function () {
-		borrarArticulo(itemID, clon, cantArt);
+	$borrar.click(function(){
+		borrarArticulo(itemID, clon);
 		return false;
-	});
-
-	if (cantArt > 4) {
-		aumAncho(); //si hay mas de 4 incrementamos el ancho 
-	}
+		//console.log(clon);
+	});	
 }
 
-borrarArticulo = (itemID, clon, cantArt) => {
+borrarArticulo = (itemID, clon) => {
 	const item = $("#" + itemID);
 	const stockItem = item.children("label.stock").text();
 
 	clon.remove();//borramos el artículo
-	compras--;
+	compras--; 
 	actualizarCompra(compras); //actualizar carro
 	stock = parseInt(stockItem.substring(6));
 	sumarItem = false;
 	actualizarStock(itemID, stock, sumarItem); //volver a actualizar el stock
 	actualizarTotal(itemID, sumarItem); //restar pre.articulo al total
-
-	if (cantArt > 4){ 
-		reducirAncho(); //si hay mas de 4 decrementamos el ancho 
-	}else{
-		$("#cart_items").css("left","0px");
-		$("#cart_items").css("width", anchoInicial);
-	}
 }
-
-
-vaciarCarrito = () => {
-	const deletes = $("#cart_items").children(".item").children(".delete"); //seleccionamos los botones de borrar item
-	deletes.trigger("click"); //lanzamos un trigger de su listener para ejecutarlos todos
-	$("#cart_items").css("left","0px");
-	$("#cart_items").css("width", anchoInicial+"px");
-}
-
-aumAncho = () => {
-	const ancho = parseInt($("#cart_items").css("width"));
-	$("#cart_items").css("width", ancho + 120 + "px");
-}
-
-redAncho = () => {
-	const ancho = parseInt($("#cart_items").css("width"));
-	$("#cart_items").css("width", ancho - 120 + "px");
-	movIquierda();
-}
-
-movDerecha = () => {
-	const ancho = parseInt($("#cart_items").css("width"));
-	const left = parseInt($("#cart_items").css("left"))
-	if (compras > 4){
-		if ((left+ancho)>anchoInicial) {
-			$("#cart_items").css("left", left - 60 + "px");
-		}	
-	}
-}
-
-movIquierda = () => {
-	const left = parseInt($("#cart_items").css("left"));
-	if (compras > 4 || left < 0){
-		if (left != 0){
-			$("#cart_items").css("left",left + 60 + "px");
-		}	
-	}
-}
-
-
-
