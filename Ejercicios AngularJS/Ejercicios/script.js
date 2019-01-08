4bfc56ad231cf3af85603e3b912cf548a860ea55
Erik Martorell Gomez <erikmarto12@gmail.com>
2019-01-08 13:21:23 +0100

    AngularJS

--- Ejercicios AngularJS/Ejercicios/script.js
+++ Ejercicios AngularJS/Ejercicios/script.js
@@ -1,30 +1,39 @@
-var app=angular.module("app", [])
-    .controller("HipotecaController", function($scope){
+var app = angular.module("app", [])
+    .controller("HipotecaController", function ($scope) {
         $scope.hipoteca = {
-            idHipoteca:3,
-            nombre:"Juan",
-            ape1:"García",
-            ape2:"Perez",
-            nif:"12345678Z",
-            edad:32,
-            telefono:"964564563",
-            email:"juan_garcia_perez@gmail.com",
-            ingresosMensuales:1200,
-            cuotaMensual:492,
-            interesAplicado:0.987,
+            idHipoteca: 3,
+            nombre: "Juan",
+            ape1: "García",
+            ape2: "Perez",
+            nif: "12345678Z",
+            edad: 32,
+            telefono: "964564563",
+            email: "juan_garcia_perez@gmail.com",
+            ingresosMensuales: 1200,
+            cuotaMensual: 492,
+            interesAplicado: 0.987,
             datosFinancieros: {
-                capital:150000,
-                tipoInteres:"VARIABLE",
-                tipoEuribor:"EURIBOR3",
-                euribor:0.287,
-                diferencial:0.9,
-                interesFijo:0,
-                plazoAnyos:30
+                capital: 150000,
+                tipoInteres: "VARIABLE",
+                tipoEuribor: "EURIBOR3",
+                euribor: 0.287,
+                diferencial: 0.9,
+                interesFijo: 0,
+                plazoAnyos: 30
             },
             productosContratados: {
-                seguroCasa:true,
-                nomina:true,
-                seguroVida:false
+                seguroCasa: true,
+                nomina: true,
+                seguroVida: false
             }
         }
+        $scope.hipotecaModelo = {
+            datosFinancieros: {
+                tipoInteres: ["VARIABLE", "FIJO"],
+                tipoEuribor: ["EURIBOR", "EURIBOR3"]
+            }
+        }
+        app.controller("PruebaController", function ($scope) {
+            $scope.url = "detalle.html";
+        });
     });
