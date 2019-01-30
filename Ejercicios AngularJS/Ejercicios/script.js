0ffe83842aecc1a9129f8cf3fb35d6f3a51582aa
erikmarto <erikmarto12@gmail.com>
2019-01-30 08:35:52 +0100

    AngularJs cambios

--- Ejercicios AngularJS/Ejercicios/script.js
+++ Ejercicios AngularJS/Ejercicios/script.js
@@ -1,40 +1,9 @@
-var app = angular.module("app", [])
-    .controller("HipotecaController", function ($scope) {
-        $scope.hipoteca = {
-            idHipoteca: 3,
-            nombre: "",
-            ape1: "",
-            ape2: "",
-            nif: "",
-            edad: 0,
-            telefono: "",
-            email: "",
-            ingresosMensuales: 0,
-            cuotaMensual: 0,
-            interesAplicado: 0,
-            datosFinancieros: {
-                capital: 0,
-                tipoInteres: "",
-                tipoEuribor: "",
-                euribor: 0,
-                diferencial: 0,
-                interesFijo: 0,
-                plazoAnyos: 0
-            },
-            productosContratados: {
-                seguroCasa: false,
-                nomina: false,
-                seguroVida: false
-            }
-        }
+var app = angular.module("app", []);
 
-        $scope.hipotecaModelo = {
-            datosFinancieros: {
-                tipoInteres: ["VARIABLE", "FIJO"],
-                tipoEuribor: ["EURIBOR", "EURIBOR3"]
-            }
-        }
-        $scope.url = "detalle.html";
-    });
+app.constant("miServicioConstante", "Hola mundo");
+
+app.controller("PruebaController", ["$scope", "miServicioConstante", function ($scope, miServicioConstante) {
+    $scope.valor = miServicioConstante;
+}]);
 
 
