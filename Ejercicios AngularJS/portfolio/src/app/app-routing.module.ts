652e79c59b84adf8305d2763e149aa6e002d9ca2
erikmarto <erikmarto12@gmail.com>
2019-02-11 09:52:04 +0100

    Modificando portfolio

--- Ejercicios AngularJS/portfolio/src/app/app-routing.module.ts
+++ Ejercicios AngularJS/portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PortfolioItemComponent } from './pages/portfolio-item/portfolio-item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'home', component: PortfolioComponent},
+  {path : 'about', component: AboutComponent},
+  {path : 'item', component: PortfolioItemComponent},
+  {path : '**', pathMatch:'full', redirectTo:'home'}
+  //si escribes cualquier ruta que sea distinta redirecciona al path home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
